9220f87883e9750f7ff9c2d7496e41fef609515d
ناصر خشمان عبدالله <nasserkhashman5@gmail.com>
2022-07-30 15:30:04 +0300

    done

---
+++ My project (4)/Assets/car.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car : MonoBehaviour 
+{
+    // Start is called before the first frame update
+    int age = 17;
+    string name = "asta";
+    int height = 169;
+    string superpower = "blackmgic";
+        string name2 = "zoro";
+    int age2 = 27;
+
+    int height2 = 184;
+
+
+    string superpower2 = "devill swords";
+    void Start()
+    {
+       
+        print("my name is " + name + "my age is " + age + "my height is " + height + "my superpower" + superpower);
+
+
+      
+
+        print("my name is" + name2 + "my name is" + age2 + "my height is " + height2 + "my superpower is" + superpower2);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
